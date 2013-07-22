6f4c26cc08cad36a6d11e02e397b265fd82d0a2b
iceleaf916 <kaisheng.ye@gmail.com>
2013-07-22 22:35:51 +0800

    update

--- album/models.py
+++ album/models.py
@@ -13,7 +13,7 @@ class Software(models.Model):
     display_name = models.CharField(max_length=255, verbose_name='显示名称')
     short_desc = models.CharField(max_length=2550, verbose_name='一句话描述')
     long_desc = models.TextField(max_length=2550, verbose_name='推荐简介')
-    software_pic = models.ImageField(upload_to=ALBUM_SOFTWARE_PIC_PATH, verbose_name="软件宣传图片")
+    software_pic = models.ImageField(upload_to=ALBUM_SOFTWARE_PIC_PATH, verbose_name="软件宣传图片", blank=True)
     order = models.IntegerField(blank=True, null=True, verbose_name="排序")
 
     def __unicode__(self):
@@ -27,8 +27,9 @@ class Album(models.Model):
     language = models.CharField(max_length=25, choices=LANGUAGE_CHOICE, default='en_us', verbose_name="语言")
     name = models.CharField(max_length=255, verbose_name="专辑名称")
     summary = models.TextField(max_length=2550, verbose_name="专辑简介")
-    cover_pic = models.ImageField(upload_to=ALBUM_COVER_PIC_PATH, verbose_name="专辑封面")
+    cover_pic = models.ImageField(upload_to=ALBUM_COVER_PIC_PATH, verbose_name="专辑封面", blank=True)
     order = models.IntegerField(blank=True, null=True, verbose_name="排序")
+    last_modified = models.DateTimeField(auto_now_add=True, verbose_name="发布时间")
 
     softwares = models.ManyToManyField(Software, verbose_name=u"软件列表", blank=True)
 
@@ -49,7 +50,7 @@ class AlbumPublish(models.Model):
     language = models.CharField(max_length=25, choices=LANGUAGE_CHOICE, default='en_us', verbose_name="语言")
     name = models.CharField(max_length=255, verbose_name="发布名称")
     status = models.IntegerField(choices=STATUS_CHOICE, default=1, verbose_name="状态")
-    created = models.DateTimeField(auto_now=True, auto_now_add=True)
+    pub_datetime = models.DateTimeField(verbose_name="发布时间")
 
     albums = models.ManyToManyField(Album, verbose_name=u"要发布的专辑", blank=True)
 
@@ -59,7 +60,7 @@ class AlbumPublish(models.Model):
     class Meta:
         verbose_name = "发布管理"
         verbose_name_plural = "发布管理"
-        ordering = ['-created'] 
+        ordering = ['-pub_datetime'] 
 
     @property
     def is_test(self):
